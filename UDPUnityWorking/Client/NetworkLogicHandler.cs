e1759cf70c8710f62efdea1db8d6ca63d277ff0f
sahillshaikh1 <50543009+sahillshaikh1@users.noreply.github.com>
2022-12-20 16:25:25 +0530

    Update NetworkLogicHandler.cs

--- UDPUnityWorking/Client/NetworkLogicHandler.cs
+++ UDPUnityWorking/Client/NetworkLogicHandler.cs
@@ -29,12 +29,55 @@ public class NetworkLogicHandler : MonoBehaviour
     public void DataHandler()
     {
         num++;
-        UdpClientTest.instance.sendMsg("Hii Sahil" + num, "Hii Back", "Hii Back");
+      //  UdpClientTest.instance.sendMsg("Hii Sahil" + num, "Hii Back", "Hii Back");
 
     }
 
     private void Update()
     {
         textdEBUG.text = UdpClientTest.instance.GetResponce();
+        if (Authentication.instance.userTaken)
+        {
+            Authentication.instance.ErrorText.color = Color.red;
+            Authentication.instance.ErrorText.text = "This username is already taken.Try Another";
+        }
+        else
+        {
+            Authentication.instance.ErrorText.text = "";
+        }
+    }
+    public void ProcessingData(string responce)
+    {
+        if (string.IsNullOrEmpty(responce))
+        {
+            return;
+        }
+        Debug.Log(responce + " " + responce.Substring(0, 4));
+        if (responce.Substring(0, 9) == "USERTAKEN") /// REGISTER LOGIC
+        {
+           
+            Authentication.instance.userTaken = true;
+          
+            Debug.Log("BHAI NAAM TAKEN HAI");
+
+        } 
+        if (responce == "USERNOTAKEN") /// REGISTER LOGIC
+        {
+           
+            Authentication.instance.userTaken = false;
+          
+            Debug.Log("REGISTER SUCESSFULLY");
+
+        }
+        if (responce == "LOGINSUCESS") /// Login LOGIC
+        {
+
+            Authentication.instance.islogin = true;
+          
+            Debug.Log("ILOGINSUCESS");
+
+        }
+      
+
     }
 }
