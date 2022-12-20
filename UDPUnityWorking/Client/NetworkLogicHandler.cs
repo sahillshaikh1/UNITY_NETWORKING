3a4b613891a2499def33c3bfc2347678b7703fc1
sahillshaikh1 <50543009+sahillshaikh1@users.noreply.github.com>
2022-12-20 11:49:42 +0530

    Client server -request, responce tested

---
+++ UDPUnityWorking/Client/NetworkLogicHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkLogicHandler : MonoBehaviour
+{
+
+    private static NetworkLogicHandler _instance;
+    public static NetworkLogicHandler instance { get { return _instance; } }
+
+
+    public IPEndPoint EPAddress;
+
+    public Text textdEBUG;
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    int num;
+    public void DataHandler()
+    {
+        num++;
+        UdpClientTest.instance.sendMsg("Hii Sahil" + num, "Hii Back", "Hii Back");
+
+    }
+
+    private void Update()
+    {
+        textdEBUG.text = UdpClientTest.instance.GetResponce();
+    }
+}
